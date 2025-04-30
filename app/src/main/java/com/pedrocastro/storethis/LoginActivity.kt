ed493aa21636ad2664a29f6fa0c461bca596cc91
PmasCastro <pmas.castro@gmail.com>
2025-05-01 00:52:34 +0100

    minor changes

--- app/src/main/java/com/pedrocastro/storethis/LoginActivity.kt
+++ app/src/main/java/com/pedrocastro/storethis/LoginActivity.kt
@@ -1,14 +1,10 @@
 package com.pedrocastro.storethis
 
-import android.content.SharedPreferences.Editor
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.EditText
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class LoginActivity : AppCompatActivity() {
 
